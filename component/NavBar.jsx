08424ff1addec697fc620750b0a8eefd93206318
HeatherEver <heather.eva93@gmail.com>
2022-01-25 15:43:59 +0000

    final styling added, made buttons touchable

--- component/NavBar.jsx
+++ component/NavBar.jsx
@@ -1,25 +1,33 @@
-import { Image, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Image, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 const NavBar = () => {
   return (
     <View style={styles.container}>
       <View style={styles.iconsLayout}>
-        <Image
-          source={require('../assets/NavIcons/home1.png')}
-          style={styles.icons}
-        />
-        <Image
-          source={require('../assets/NavIcons/map1.png')}
-          style={styles.icons}
-        />
-        <Image
-          source={require('../assets/NavIcons/search1.png')}
-          style={styles.icons}
-        />
-        <Image
-          source={require('../assets/NavIcons/plus1.png')}
-          style={styles.icons}
-        />
+        <TouchableOpacity>
+          <Image
+            source={require('../assets/NavIcons/home1.png')}
+            style={styles.icons}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            source={require('../assets/NavIcons/map1.png')}
+            style={styles.icons}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            source={require('../assets/NavIcons/search1.png')}
+            style={styles.icons}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            source={require('../assets/NavIcons/plus1.png')}
+            style={styles.icons}
+          />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -30,6 +38,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#253334',
     bottom: 0,
     minHeight: '12%',
+    borderColor: '#2f4d40',
+    borderWidth: 1,
     minWidth: '100%',
     borderRadius: 40,
     alignItems: 'center',
