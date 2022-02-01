002409a1bc08c09bd0c9160cdbddfb18a1399b46
salah <salah.shihaibi0@gmail.com>
2022-02-01 09:42:32 +0000

    Home page in progress

--- Server/PostsData.js
+++ Server/PostsData.js
@@ -19,32 +19,50 @@ import {
 const createPost = (postInfo) => {
   addDoc(collection(firestore, "posts"), postInfo)
     .then((post) => {
-      console.log(post);
       return post;
     })
     .catch((err) => alert(err.message));
 };
 
-const getPosts = (page) => {
+const getPosts = (page, topics) => {
+  const topicsArr = topics || [];
   const postArray = [];
-  return getDocs(
-    query(
-      collection(firestore, "posts"),
-      orderBy("createdDate"),
-      startAfter(page),
-      limit(9)
+  if (topicsArr.length) {
+    return getDocs(
+      query(
+        collection(firestore, "posts"),
+        where("topics", "array-contains-any", topics),
+        orderBy("createdDate", "desc"),
+        startAfter(page),
+        limit(10)
+      )
     )
-  )
-    .then((arr) => {
-      arr.forEach((doc) => {
-        const docData = doc.data();
-        postArray.push({ ...docData, id: doc.id });
-      });
-
-      console.log("page: ", page);
-      return postArray;
-    })
-    .catch((err) => alert(err.message));
+      .then((arr) => {
+        arr.forEach((doc) => {
+          const docData = doc.data();
+          postArray.push({ ...docData, id: doc.id });
+        });
+        return postArray;
+      })
+      .catch((err) => console.log(err.message));
+  } else {
+    return getDocs(
+      query(
+        collection(firestore, "posts"),
+        orderBy("createdDate", "desc"),
+        startAfter(page),
+        limit(10)
+      )
+    )
+      .then((arr) => {
+        arr.forEach((doc) => {
+          const docData = doc.data();
+          postArray.push({ ...docData, id: doc.id });
+        });
+        return postArray;
+      })
+      .catch((err) => alert(err.message));
+  }
 };
 
 export { createPost, getPosts };
