e9b5f073e8ee92e9f5f8ddd4b71f5582be4567c5
khanson27 <hansonkm94@gmail.com>
2022-02-01 16:45:23 +0000

    single post page styling started

--- component/SinglePost.jsx
+++ component/SinglePost.jsx
@@ -1,6 +1,13 @@
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 import { useEffect, useState } from "react";
-
 import { getSinglePost } from "../Server/PostsData";
 
 export const SinglePost = ({ navigation, route }) => {
@@ -16,8 +23,70 @@ export const SinglePost = ({ navigation, route }) => {
   }, [postId]);
 
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <ImageBackground
+      source={require("../assets/backgroundlogin.png")}
+      resizeMode="cover"
+      style={styles.Background}
+      blurRadius={5}
+    >
+      <View style={styles.container}>
+        {isLoading ? (
+          <Text>Post is Loading...</Text>
+        ) : (
+          <View>
+            <Image style={styles.image} source={{ uri: singlePost.image }} />
+            <TouchableOpacity>
+              <Text style={styles.text}>{singlePost.author}</Text>
+            </TouchableOpacity>
+            <View style={styles.locationTextContainer}>
+              <Image
+                style={styles.locationIcon}
+                source={require("../assets/location-pin.png")}
+              />
+              <Text style={styles.locationText}>{singlePost.locationName}</Text>
+            </View>
+            <Text styles={styles.text}>{singlePost.description}</Text>
+            <Text>{singlePost.likes}</Text>
+          </View>
+        )}
+      </View>
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: 400,
+    backgroundColor: "#FCFFEF",
+    marginVertical: 5,
+    marginHorizontal: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    flexDirection: "row",
+  },
+  image: {
+    height: 300,
+    width: 300,
+  },
+  text: {
+    fontSize: 15,
+  },
+  Background: {
+    height: "100%",
+    width: "100%",
+  },
+  locationTextContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  locationIcon: {
+    height: 24,
+    width: 24,
+  },
+  locationText: {
+    color: "#7C9A92",
+    fontSize: 13,
+    marginRight: 5,
+  },
+});
